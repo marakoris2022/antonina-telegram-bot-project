b336673ab6499b575f1f76f41cd95e901c0bfebf
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2025-05-11 00:25:55 +0300

    added some frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Box, CssBaseline, InitColorSchemeScript } from '@mui/material';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import Header from '@/front/components/Header';
+import Footer from '@/front/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Antonina Fitness Instructor',
@@ -18,15 +15,21 @@ export const metadata: Metadata = {
     "Welcome to Antonina's Fitness web-site! Here you can find all the information about my fitness classes, personal training, and nutrition consulting services.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang='en' suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript attribute='class' />
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <CssBaseline />
+          <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Header />
+            <Box component='main' sx={{ flex: 1 }}>
+              {props.children}
+            </Box>
+            <Footer />
+          </Box>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
